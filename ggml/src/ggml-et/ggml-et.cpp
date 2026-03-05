c0d7a1fef63ab76a28a5e0c436394af252cd717a
marty1885 <marty188586@gmail.com>
2026-03-05 07:37:07 +0000

    update limitations

--- ggml/src/ggml-et/ggml-et.cpp
+++ ggml/src/ggml-et/ggml-et.cpp
@@ -35,7 +35,7 @@ namespace fs = std::experimental::filesystem;
 
 /*
  * ggml_et_dump_tensor_metadata
- * @brief prints the metadata of a single tensor
+ * @brief prints the metadata of a single tensorf
  */
 static void ggml_et_dump_tensor_metadata(const ggml_tensor* ggtensor, size_t indent_level, const char* title)
 {
@@ -599,19 +599,18 @@ static bool ggml_backend_et_device_supports_op(ggml_backend_dev_t dev, const ggm
         case GGML_OP_MUL_MAT:
             // Support Q8_0 x F32 -> F32, F16 x F32 -> F32, and F32 x F32 -> F32 matrix multiplication
             // Stride requirements: first dimension must be contiguous for all tensors
-            if(op->type == GGML_TYPE_F32) {
+            if(op->type == GGML_TYPE_F32 && op->src[0]->type == GGML_TYPE_F32 && op->src[1]->type == GGML_TYPE_F32
+                && op->src[0]->ne[1] % 16 == 0 // m
+                && op->src[0]->ne[0] % 16 == 0 &&  // k
+                ggml_is_contiguous(op->src[0]) &&
+                ggml_is_contiguous(op->src[1])) {
                 // Special path for the FP32 TensorFMA kernel
                 // Limitation - N is free but the other dims must be multiple of 16
                 // The m edge is difficult to do because of the 4 conseqtive load hardware limitation
                 // And the k edge is impossible because that is encoded as `stride & 0xFFFFFFFFFFC0ULL` which becomes 0 for stride 16 (4x FP32) :(
                 // Can loosen but no real way to make them free. The n edge is the only free one
-                int64_t m = op->src[0]->ne[1];
-                int64_t k = op->src[0]->ne[0];
-                if(op->src[0]->type == GGML_TYPE_F32 && op->src[1]->type == GGML_TYPE_F32 && m % 16 == 0 && k % 16 == 0) {
-                    supported = true;
-                } else {
-                    supported = false;
-                }
+                // FIXME: Right now this overwrites the mul_mat_f32 kernel - whatever. Fix later. Demo code
+                supported = true;
             }
             else if (op->type == GGML_TYPE_F32 &&
                 op->src[0] && (op->src[0]->type == GGML_TYPE_Q8_0 || op->src[0]->type == GGML_TYPE_F16 || op->src[0]->type == GGML_TYPE_F32) &&
